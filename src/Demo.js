1314202bdcaab647d35cf2f4e1178b6124403094
hzk93 <hassanzk93@gmail.com>
2022-05-20 12:37:39 +0500

    React Course

---
+++ src/Demo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    constructor(props){
+        super(props);
+        console.log(this.props);
+    }
+    
+    render(){
+        return <h1>DEMO COMPONENT!</h1>;
+    }
+}
+
+export default Demo; 
